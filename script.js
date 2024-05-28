935e8d196b42d8638ffb0549881d1dadb7a9c009
RYAN LEONARDO FERNANDES MARTINS <00001073887352sp@aluno.educacao.sp.gov.br>
2024-05-28 11:20:26 -0300

    terminar mais tarde

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-!alert 
+function somarNumeros(){
+    var numero1 = document.getElementById("num1").value;
+    var numero2 = document.getElementById("num2").value;
+    var resultado = numero1+numero2
+}
